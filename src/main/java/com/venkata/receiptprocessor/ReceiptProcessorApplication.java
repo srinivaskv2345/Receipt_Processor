2fe4f073c05594880f3f5eb1e374261fea7d5da4
srinivaskv2345 <srinivaskv2345@gmail.com>
2024-01-19 16:02:59 -0500

    Initial commit

--- src/main/java/com/venkata/receiptprocessor/ReceiptProcessorApplication.java
+++ src/main/java/com/venkata/receiptprocessor/ReceiptProcessorApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-ReceiptProcessorApplication {
+public class ReceiptProcessorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReceiptProcessorApplication.class, args);
 	}
+
 
 }
